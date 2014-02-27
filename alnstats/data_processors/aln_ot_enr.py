4cdecfd90b1a6684a35c5adcced87a006258a99a
Mark <murphy.mark.w@gmail.com>
2014-02-27 12:46:38 -0600

    Now reproduces the enrichment of the previous version.

--- alnstats/data_processors/aln_ot_enr.py
+++ alnstats/data_processors/aln_ot_enr.py
@@ -8,4 +8,14 @@ class AlnOTEnrDataProcessor(dataprocessor.DataProcessor):
 
   @classmethod
   def process(cls,data,**kwargs):
-    data['aln_ot_enr'] = float(data['len'] * data['align']) / float(data['aln_ot'])
+    # Get target sizes.
+    genome = 3000000000 # TODO This should be an input quantity...
+    target = int(data['target'])
+    # Get number of bp mapped to each target.
+    rm_mapped_bp = int(data['len']) * int(data['rmdup'])
+    on_target_bp = int(data['aln_ot'])
+    # Calculate fraction of each.
+    frac_sizes = float(genome) / float(target)
+    frac_reads = float(rm_mapped_bp) / float(on_target_bp)
+    # Compare.
+    data['aln_ot_enr'] = frac_sizes / frac_reads
--- alnstats/data_processors/seq_ot_enr.py
+++ alnstats/data_processors/seq_ot_enr.py
@@ -8,4 +8,14 @@ class SeqOTEnrDataProcessor(dataprocessor.DataProcessor):
 
   @classmethod
   def process(cls,data,**kwargs):
-    data['seq_ot_enr'] = float(data['len'] * data['reads']) / float(data['aln_ot'])
+    # Get target sizes.
+    genome = 3000000000 # TODO This should be an input quantity...
+    target = int(data['target'])
+    # Get number of bp for each target.
+    sequenced_bp = int(data['len']) * int(data['reads'])
+    on_target_bp = int(data['aln_ot'])
+    # Calculate fraction of each.
+    frac_sizes = float(genome) / float(target)
+    frac_reads = float(sequenced_bp) / float(on_target_bp)
+    # Compare.
+    data['seq_ot_enr'] = frac_sizes / frac_reads
